69a05ae8026a3ffe2ed13ec8fc13e1e2cf04edec
stiguer <sabertiguer@gmail.com>
2019-02-26 11:41:07 +0100

    xmltodict

--- webclient.py
+++ webclient.py
@@ -2,6 +2,8 @@
 
 from urllib.request import urlopen
 import bs4
+import xmltodict
+import pprint
 
 class WebClient(object):
     """WebClient class"""
@@ -10,26 +12,33 @@ class WebClient(object):
 
     def download_page(arg):
         # connect to the web site
-        f = urlopen("https://www.banggood.com/es/Flashdeals.html")
+        f = urlopen("https://api.openweathermap.org/data/2.5/weather?q=LLeida,es&appid=0c5943338e78117b61d5a9249ba17b82&mode=xml&unit=metric")
         # get the download_page
         page = f.read()
         # close the connection
         f.close()
         return page
 
-    def search_activities(self, page):
-        tree = bs4.BeautifulSoup(page, "lxml")
+    #def search_activities(self, page):
+    #    tree = bs4.BeautifulSoup(page, "lxml")
+    #
+    #    ul = tree.find("ul", "goodlist_1")
+    #    li = ul.find_all("li")
+    #    act_list = []
 
-        ul = tree.find("ul", "goodlist_1")
-        li = ul.find_all("li")
-        act_list = []
-
-        for item in li:
+    #    for item in li:
             # process item
-            price = item.find("span", "price").text
-            title = item.find("span", "title").text
-            act_list.append((title, price))
-        return act_list
+    #        price = item.find("span", "price").text
+    #        title = item.find("span", "title").text
+    #        act_list.append((title, price))
+    #    return act_list
+
+    def search_activities(self, page):
+        xml = xmltodict.parse(page)
+        pprint.pprint(xml)
+        temp = xml['current']['temperature']['@value']
+        weather = xml['current']['weather']['@value']
+        return temp+" and "+weather
 
 
     def run(self):
